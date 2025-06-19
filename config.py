5e9aed332fc0dd7e8a61b02214bebf2f22a0d90c
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-06-19 16:10:00 -0300

    Criar config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Admin principal
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+# Lista de IDs autorizados (convertido de string para int)
+raw_ids = os.getenv("ALLOWED_IDS", "")
+AUTHORIZED_USERS = [int(uid.strip()) for uid in raw_ids.split(",") if uid.strip().isdigit()]
